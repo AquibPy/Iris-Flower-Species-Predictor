efe9fc27e79537b373b6e9730f6013f28f64bf84
AquibPy <mohdaquib31@gmail.com>
2021-10-08 15:16:20 +0530

    added Dockerfile

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def predict():
         output = 'Virginica'
     return render_template('index.html',prediction_text='Species is {}'.format(output))
     
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
